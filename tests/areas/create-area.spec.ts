e8caabeced403e3408d32f806279b811e2df652c
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-05-29 20:39:54 -0300

    Adiciona teste para criação de área com limite de caracteres

--- tests/areas/create-area.spec.ts
+++ tests/areas/create-area.spec.ts
@@ -18,3 +18,13 @@ test('Sad Path: Create a new area with link', async ({ page }) => {
 
     await expect(page.getByRole('link', { name: 'Link suspeito' })).not.toBeVisible();
 });
+
+test('Edge Path: Create a new area with a character limit', async ({ page }) => {
+    const areaPage = new AreaPage(page);
+
+    await areaPage.goto();
+    await areaPage.createArea('A'.repeat(126));
+    await areaPage.submit();
+
+    await expect(page.getByText('O campo nome da área não pode ser superior a 125 caracteres.')).toBeVisible();
+});
