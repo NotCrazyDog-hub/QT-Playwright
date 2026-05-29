f752891b2dcd8c9e25a5741cd78f294e99b5ce81
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-05-29 20:15:27 -0300

    Adiciona criação de área com data e caso triste com link suspeito

--- tests/areas/create-area.spec.ts
+++ tests/areas/create-area.spec.ts
@@ -1,10 +1,20 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { AreaPage } from '../../pages/AreaPage';
 
 test('Happy Path: Create a new area', async ({ page }) => {
     const areaPage = new AreaPage(page);
 
     await areaPage.goto();
-    await areaPage.createArea('Área 51 brasileira');
+    await areaPage.createArea('Área 51 brasileira ' + Date.now());
     await areaPage.submit();
+});
+
+test('Sad Path: Create a new area with link', async ({ page }) => {
+    const areaPage = new AreaPage(page);
+
+    await areaPage.goto();
+    await areaPage.createArea('<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Link suspeito</a>');
+    await areaPage.submit();
+
+    await expect(page.getByRole('link', { name: 'Link suspeito' })).not.toBeVisible();
 });
