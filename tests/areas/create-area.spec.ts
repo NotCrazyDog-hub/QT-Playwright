77d45be3478096527f4b3934a2e6b7309ac9a4e0
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-05-29 20:41:06 -0300

    Remove linhas em branco nos testes de criação de área

--- tests/areas/create-area.spec.ts
+++ tests/areas/create-area.spec.ts
@@ -3,7 +3,6 @@ import { AreaPage } from '../../pages/AreaPage';
 
 test('Happy Path: Create a new area', async ({ page }) => {
     const areaPage = new AreaPage(page);
-
     await areaPage.goto();
     await areaPage.createArea('Área 51 brasileira ' + Date.now());
     await areaPage.submit();
@@ -11,20 +10,16 @@ test('Happy Path: Create a new area', async ({ page }) => {
 
 test('Sad Path: Create a new area with link', async ({ page }) => {
     const areaPage = new AreaPage(page);
-
     await areaPage.goto();
     await areaPage.createArea('<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Link suspeito</a>');
     await areaPage.submit();
-
     await expect(page.getByRole('link', { name: 'Link suspeito' })).not.toBeVisible();
 });
 
 test('Edge Path: Create a new area with a character limit', async ({ page }) => {
     const areaPage = new AreaPage(page);
-
     await areaPage.goto();
     await areaPage.createArea('A'.repeat(126));
     await areaPage.submit();
-
     await expect(page.getByText('O campo nome da área não pode ser superior a 125 caracteres.')).toBeVisible();
 });
