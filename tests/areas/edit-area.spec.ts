6bd680a76d765a51598d906b032b25768f3079a6
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2026-06-01 14:29:06 -0300

    Adiciona verificação de sucesso após editar uma área no teste de caminho feliz e ajusta o teste de caminho triste para garantir a validação correta do nome vazio.

--- tests/areas/edit-area.spec.ts
+++ tests/areas/edit-area.spec.ts
@@ -12,6 +12,23 @@ test('Happy Path: Edit the name of an existing area', async ({ page }) => {
     await expect(page.getByText('Área salva com sucesso')).toBeVisible();
 
     await areaPage.editArea(originalName, editedName);
+    await areaPage.submit();
 
     await expect(page.getByText('Área salva com sucesso')).toBeVisible();
+});
+
+test('Sad Path: Edit an area to have an empty name.', async ({ page }) => {
+    const areaPage = new AreaPage(page);
+    const originalName = 'Área Link ' + Date.now();
+    const editedName = '       ';
+
+    await areaPage.goto();
+    await areaPage.createArea(originalName);
+    await areaPage.submit();
+    await expect(page.getByText('Área salva com sucesso')).toBeVisible();
+
+    await areaPage.editArea(originalName, editedName);
+    await areaPage.submit();
+
+    await expect(page.getByText('Este campo não pode conter apenas espaços em branco')).toBeVisible();
 });
