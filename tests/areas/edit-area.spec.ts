91aa5ef199b849b74b5940f88872ee464791b9ed
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2026-06-01 14:44:55 -0300

    Corrige descrição do teste "Sad Path" e adiciona teste de borda para caso de caracteres especiais

--- tests/areas/edit-area.spec.ts
+++ tests/areas/edit-area.spec.ts
@@ -17,7 +17,7 @@ test('Happy Path: Edit the name of an existing area', async ({ page }) => {
     await expect(page.getByText('Área salva com sucesso')).toBeVisible();
 });
 
-test('Sad Path: Edit an area to have an empty name.', async ({ page }) => {
+test('Sad Path: Edit an area to have an empty name', async ({ page }) => {
     const areaPage = new AreaPage(page);
     const originalName = 'Área Link ' + Date.now();
     const editedName = '       ';
@@ -31,4 +31,20 @@ test('Sad Path: Edit an area to have an empty name.', async ({ page }) => {
     await areaPage.submit();
 
     await expect(page.getByText('Este campo não pode conter apenas espaços em branco')).toBeVisible();
+});
+
+test('Edge Case: Edit an area to have a name with special characters', async ({ page }) => {
+    const areaPage = new AreaPage(page);
+    const originalName = 'Área no seu limite ' + Date.now();
+    const editedName = '!@#$%¨&**';
+
+    await areaPage.goto();
+    await areaPage.createArea(originalName);
+    await areaPage.submit();
+    await expect(page.getByText('Área salva com sucesso')).toBeVisible();
+
+    await areaPage.editArea(originalName, editedName);
+    await areaPage.submit();
+
+    await expect(page.getByText('Conteúdo inválido detectado na requisição.')).toBeVisible();
 });
