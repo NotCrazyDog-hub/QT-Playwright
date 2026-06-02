377681f288a9e86d9aa611a02695dbfeab1f9876
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-06-02 14:53:30 -0300

    Adiciona testes para criação de turma com cenários de erro e ano inválido

--- tests/turmas/create-turma.spec.ts
+++ tests/turmas/create-turma.spec.ts
@@ -8,3 +8,19 @@ test('Happy Path: Create a new turma', async ({ page }) => {
     await turmaPage.submit();
     await expect(page.getByText('Turma salva com sucesso')).toBeVisible();
 });
+
+test('Sad Path: Create a turma without filling in one of the required fields', async ({ page }) => {
+    const turmaPage = new TurmaPage(page);
+    await turmaPage.goto();
+    await turmaPage.createTurma('', '2024', '1ª Série / 1º Semestre', 'Vespertino');
+    await turmaPage.submit();
+    await expect(page.getByText('Este campo é obrigatório')).toBeVisible();
+});
+
+test('Edge Path: Create a new turma with an invalid year', async ({ page }) => {
+    const turmaPage = new TurmaPage(page);
+    await turmaPage.goto();
+    await turmaPage.createTurma('Fisioterapia', '2028', '1ª Série / 1º Semestre', 'Vespertino');
+    await turmaPage.submit(); 
+    await expect(page.getByText('O ano não pode ser superior a 2027.')).toBeVisible();
+});
