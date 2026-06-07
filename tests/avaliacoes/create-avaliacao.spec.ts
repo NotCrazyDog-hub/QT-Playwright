74be6714a5c8975abe3b78a37991c96708336270
NotCrazyDog-hub <pedro.oliviera.ncd@gmail.com>
2026-06-07 16:00:08 -0300

    Adiciona testes para criação de avaliações, incluindo cenários de sucesso, erro ao não preencher campos obrigatórios e limite máximo de caracteres.

--- tests/avaliacoes/create-avaliacao.spec.ts
+++ tests/avaliacoes/create-avaliacao.spec.ts
@@ -9,3 +9,21 @@ test('Happy Path: Create a new avaliação', async ({ page }) => {
     await avaliacaoPage.submit();
     await expect(page.getByText('Avaliação cadastrada com sucesso!')).toBeVisible();
 });
+
+test('Sad Path: Create a new avaliação without filling required fields', async ({ page }) => {
+    const avaliacaoPage = new AvaliacaoPage(page);
+    
+    await avaliacaoPage.goto();
+    await avaliacaoPage.createAvaliacao('', 'E2e Super Teacher 37', 'Filosofia');
+    await avaliacaoPage.submit();
+    await expect(page.getByText('Por favor, corrija os erros no formulário.')).toBeVisible();
+});
+
+test('Edge Path: Create a new avaliação with maximum length text', async ({ page }) => {
+    const avaliacaoPage = new AvaliacaoPage(page);
+    
+    await avaliacaoPage.goto();
+    await avaliacaoPage.createAvaliacao('Vou criar uma avaliação repleta de desafios. Esta prova refletirá cada dúvida, erro e noite de estudo. Nela, a nota máxima será uma lenda distante, enquanto questões implacáveis desafiarão os alunos numa batalha contra o tempo e o conhecimento. cruelmente.', 'E2e Super Teacher 37', 'Filosofia');
+    await avaliacaoPage.submit();
+    await expect(page.getByText('A descrição deve ter no máximo 255 caracteres')).toBeVisible();
+});
